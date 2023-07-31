c544c42e529bdf0f520c26f21053c3272f2c2c30
asd45618 <asd45618@naver.com>
2023-07-31 17:53:15 +0900

    싸이월드

---
+++ class/03-javascript/연습-07-math.js
@@ -0,0 +1,29 @@
+Math.floor(Math.random() * 1000000);
+// 480510
+
+String(Math.floor(Math.random() * 1000000));
+// '228735'
+
+String(Math.floor(Math.random() * 1000000)).padStart(6, "0");
+// '794968'
+
+String(Math.floor(Math.random() * 1000000)).padStart(6, "0");
+// '334622'
+
+String(Math.floor(Math.random() * 1000000)).padStart(6, "0");
+// '410574'
+
+String(Math.floor(Math.random() * 1000000)).padStart(6, "0");
+// '183067'
+
+String(Math.floor(Math.random() * 1000000)).padStart(6, "0");
+// '002417'
+
+let result = String(Math.floor(Math.random() * 1000000)).padStart(6, "0");
+// undefined
+
+result;
+// '507916'
+
+result;
+// '507916'
