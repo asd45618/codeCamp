437ba67c7230190ee4f3e67b656dea9a11ac4382
asd45618 <asd45618@naver.com>
2023-08-04 14:30:14 +0900

    자바스크립트 내장함수

--- class/03-javascript/06-timer.js
+++ class/03-javascript/06-timer.js
@@ -1,18 +1,30 @@
+let isStarted = false;
+
 const auth = () => {
-  const token = String(Math.floor(Math.random() * 1000000)).padStart(6, "0");
-  document.getElementById("target").innerText = token;
-  document.getElementById("target").style.color = `#${token}`;
+  if (isStarted === false) {
+    //타이머가 작동 중이 아닐 때
+    isStarted = true;
+    document.getElementById("complete").disabled = false;
+    const token = String(Math.floor(Math.random() * 1000000)).padStart(6, "0");
+    document.getElementById("target").innerText = token;
+    document.getElementById("target").style.color = `#${token}`;
 
-  let time = 5;
+    let time = 5;
+    let timer;
 
-  setInterval(function () {
-    if (time >= 0) {
-      let min = Math.floor(time / 60);
-      let sec = String(time % 60).padStart(2, "0");
-      document.getElementById("timer").innerText = min + ":" + sec;
-      time = time - 1;
-    } else {
-      document.getElementById("complete").disabled = true;
-    }
-  }, 1000);
+    timer = setInterval(function () {
+      if (time >= 0) {
+        let min = Math.floor(time / 60);
+        let sec = String(time % 60).padStart(2, "0");
+        document.getElementById("timer").innerText = min + ":" + sec;
+        time = time - 1;
+      } else {
+        document.getElementById("complete").disabled = true;
+        isStarted = false;
+        clearInterval(timer);
+      }
+    }, 1000);
+  } else {
+    //타이머가 작동 중일 때
+  }
 };
